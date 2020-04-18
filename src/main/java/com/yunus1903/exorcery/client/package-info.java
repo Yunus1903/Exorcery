9569961b392b89b09f3bd62abd3faedcfa70cbef
Yunus1903 <yunus1903dj@gmail.com>
2020-04-18 22:17:16 +0200

    Added package-info.java to all packages with code in

---
+++ src/main/java/com/yunus1903/exorcery/client/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * @author Yunus1903
+ * @since 18/04/2020
+ */
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package com.yunus1903.exorcery.client;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
---
+++ src/main/java/com/yunus1903/exorcery/init/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * @author Yunus1903
+ * @since 18/04/2020
+ */
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package com.yunus1903.exorcery.init;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
