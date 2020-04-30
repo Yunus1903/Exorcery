ea67561954a8ce5236ae7ed949adead062290198
Yunus1903 <yunus1903dj@gmail.com>
2020-04-30 23:36:05 +0200

    Added Mana potion recipe

--- src/main/java/com/yunus1903/exorcery/core/CommonProxy.java
+++ src/main/java/com/yunus1903/exorcery/core/CommonProxy.java
@@ -1,8 +1,24 @@
 package com.yunus1903.exorcery.core;
 
+import com.yunus1903.exorcery.init.ModItems;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.potion.PotionUtils;
+import net.minecraft.potion.Potions;
+import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
+
 /**
  * @author Yunus1903
  */
 public class CommonProxy
 {
+    static
+    {
+        BrewingRecipeRegistry.addRecipe(
+                Ingredient.fromStacks(PotionUtils.addPotionToItemStack(new ItemStack(Items.POTION), Potions.AWKWARD)),
+                Ingredient.fromItems(Items.LAPIS_LAZULI),
+                new ItemStack(ModItems.MANA_POTION)
+        );
+    }
 }
