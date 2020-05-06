e6f683ea2b332578cd9ecad498663205efe705dc
Yunus1903 <yunus1903dj@gmail.com>
2020-05-06 06:49:35 +0200

    Added mana bar to HUD

--- src/main/java/com/yunus1903/exorcery/client/gui/ManaGui.java
+++ src/main/java/com/yunus1903/exorcery/client/gui/ManaGui.java
@@ -1,8 +1,11 @@
 package com.yunus1903.exorcery.client.gui;
 
+import com.mojang.blaze3d.platform.GlStateManager;
 import com.yunus1903.exorcery.common.capabilities.mana.ManaProvider;
 import com.yunus1903.exorcery.core.Exorcery;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.AbstractGui;
+import net.minecraft.client.gui.IngameGui;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -17,43 +20,78 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(value = Dist.CLIENT, modid = Exorcery.MOD_ID)
 public class ManaGui
 {
-    private static final ResourceLocation GUI_ICONS_LOCATION = new ResourceLocation(Exorcery.MOD_ID, "textures/gui/icons.png");
+    private static final ResourceLocation MANA_BAR_LOCATION = new ResourceLocation(Exorcery.MOD_ID, "textures/gui/mana_bar.png");
 
     @SubscribeEvent
-    public static void renderOverlay(RenderGameOverlayEvent event)
+    public static void onRenderOverlay(RenderGameOverlayEvent.Pre event)
     {
         Minecraft mc = Minecraft.getInstance();
-        /*
-        if (event.getType() == RenderGameOverlayEvent.ElementType.HEALTH)
+
+        switch (event.getType())
         {
-            mc.player.getCapability(ManaProvider.MANA_CAPABILITY).ifPresent(mana ->
-            {
-                int manaVal = (int) (mana.get() / mana.getMax() * 20);
-                int scaledWidth = mc.getMainWindow().getScaledWidth() / 2 - 91;
-
-                int i2 = MathHelper.ceil(20 / 2.0F / 10.0F);
-                i2 = (mc.player.getTotalArmorValue() > 0) ? i2 * 2 : i2;
-                int scaledHeight = mc.getMainWindow().getScaledHeight() - 39 - (i2 - 1) * Math.max(10 - (i2 - 2), 3) - 10;
-
-                mc.getProfiler().startSection("mana");
-
-                for (int i = 0; i < 10; ++i)
+            case ARMOR:
+            case HEALTH:
+            case FOOD:
+            case AIR:
+                if (!mc.player.isRidingHorse())
                 {
-                    mc.ingameGUI.blit(scaledWidth + i * 8, scaledHeight, 16, 0, 9, 9);
-                    mc.ingameGUI.blit(scaledWidth + i * 8, scaledHeight, 52, 0, 9, 9);
+                    GlStateManager.pushMatrix();
+                    GlStateManager.translatef(0, -8, 0);
                 }
-
-                mc.getProfiler().endSection();
-            });
+                break;
+            case EXPERIENCE:
+                renderMana();
+                break;
         }
-         */
+    }
 
-        if (event.getType() == RenderGameOverlayEvent.ElementType.TEXT)
+    @SubscribeEvent
+    public static void onRenderOverlay(RenderGameOverlayEvent.Post event)
+    {
+        switch (event.getType())
         {
-            mc.player.getCapability(ManaProvider.MANA_CAPABILITY).ifPresent(mana ->
-            {
-                mc.fontRenderer.drawString("Mana: " + (int) mana.get() + " / " + (int) mana.getMax(), 20, mc.getMainWindow().getScaledHeight() - 25, 0);
-            });
+            case ARMOR:
+            case HEALTH:
+            case FOOD:
+            case AIR:
+                if (!Minecraft.getInstance().player.isRidingHorse()) GlStateManager.popMatrix();
+                break;
         }
+    }
+
+    private static void renderMana()
+    {
+        Minecraft mc = Minecraft.getInstance();
+        IngameGui gui = mc.ingameGUI;
+
+        int scaledWidth = mc.getMainWindow().getScaledWidth();
+        int scaledHeight = mc.getMainWindow().getScaledHeight();
+
+        mc.player.getCapability(ManaProvider.MANA_CAPABILITY).ifPresent(mana ->
+        {
+            mc.getProfiler().startSection("manaBar");
+            mc.getTextureManager().bindTexture(MANA_BAR_LOCATION);
+
+            int x = scaledWidth / 2 - 91;
+            int y = scaledHeight - 32 + 3 - 8;
+
+            int k = (int) (mana.get() / mana.getMax() * 183.0F);
+            gui.blit(x, y, 0, 0, 182, 5);
+            if (k > 0) gui.blit(x, y, 0, 5, k, 5);
+
+            mc.getTextureManager().bindTexture(AbstractGui.GUI_ICONS_LOCATION);
+            mc.getProfiler().endStartSection("manaNumber");
+
+            String s = "" + (int) mana.get();
+            x = (scaledWidth - mc.fontRenderer.getStringWidth(s)) / 2;
+            y = scaledHeight - 31 - 4 - 8;
+            mc.fontRenderer.drawString(s, (float) (x + 1), (float) y, 0);
+            mc.fontRenderer.drawString(s, (float) (x - 1), (float) y, 0);
+            mc.fontRenderer.drawString(s, (float) x, (float) (y + 1), 0);
+            mc.fontRenderer.drawString(s, (float) x, (float) (y - 1), 0);
+            mc.fontRenderer.drawString(s, (float) x, (float) y, 0x26EEEE);
+
+            mc.getProfiler().endSection();
+        });
     }
 }
